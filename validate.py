7fa64d456c3f553a629a6d5032d8cba00eddb00d
Marc Doerflinger <marc@doerf.li>
2021-02-27 14:45:56 +0100

    add simple validate script

---
+++ validate.py
@@ -0,0 +1,17 @@
+import csv
+
+lastline = []
+
+with open('vacc_data.csv', 'r', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in csvreader:
+        lastline = row
+
+print(lastline)
+
+for f in lastline:
+    if f == '':
+        raise BaseException('empty field in last line')
+    if f == '0':
+        raise BaseException('field with value 0 in last line')
+    
